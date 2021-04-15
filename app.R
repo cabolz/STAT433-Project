db4e535d199a21bd3346df66d3761760efba8c08
Caitlin Bolz <75763155+cabolz@users.noreply.github.com>
2021-04-15 08:13:07 -0500

    Update app.R

--- app.R
+++ app.R
@@ -9,35 +9,6 @@ UberandWeather = read_csv("UberandWeather.csv")
 
 ui<-fluidPage(
   titlePanel("NYC Map"),
-  sidebarLayout(
-  mainPanel(leaflet() %>%
-              addTiles() %>%
-              setView(-74.00, 40.71, zoom = 12))  
-            )
-)
-              
-
-
-server <- function(input, output) {
-  
-
-}
-
-shinyApp(ui = ui, server = server)
-
-
-#CHECKING IF THIS UPDATES _------------------------------
-library("shiny")
-library("readr")
-library("ggplot2")
-library("dplyr")
-library("plotly")
-
-# Load in dataset
-UberandWeather = read_csv("UberandWeather.csv")
-
-ui<-fluidPage(
-  titlePanel("NYC Map"),
   
   sidebarLayout(
     mainPanel(leaflet() %>%
