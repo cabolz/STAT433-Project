4c5a16fc4537dc793286eefaddc05dccbf914e62
Matt <mcvoss@wisc.edu>
2021-04-27 11:25:05 -0500

    some changes

--- app.R
+++ app.R
@@ -8,31 +8,28 @@ library(broom)
 library(httr)
 library(rgdal)
 library(shiny)
-animals <- c("dog", "cat", "mouse", "bird", "other", "I hate animals")
-ui <- fluidPage(
-  selectInput("state", "What's your favourite state?", state.name),
-  radioButtons("animal", "What's your favourite animal?", animals)
-)
+
+#uber app
 borough<- c("NYC","Manhattan","Brooklyn","Queens","Bronx","Staten Island")
 ui <- fluidPage(
-  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
-  verbatimTextOutput("summary"),
-  tableOutput("table"),
-  sliderInput("rng","May 2014",value=c(1,31),min=1,max=31),
-  selectInput("borough","Which borough would you like to see?",borough)
+  titlePanel("NYC Uber Pickup"),
+  sidebarLayout(
+    sidebarPanel(
+    helpText("data from NYC Taxi & Limousine Commission"),
+    sliderInput("rng","May 2014",value=c(1,31),min=1,max=31),
+    selectInput("borough","Which borough would you like to see?",borough))
+  ),
+  mainPanel(plotOutput("map"))
 )
+
+
 server <- function(input, output, session) {
-  output$summary <- renderPrint({
-    dataset <- get(input$dataset, "package:datasets")
-    summary(dataset)
-  })
-  
-  output$table <- renderTable({
-    dataset <- get(input$dataset, "package:datasets")
-    dataset
+  output$map<- renderPlot({
+    
   })
 }
-shinyApp(ui, server)
 
+
+shinyApp(ui, server)
 
 
--- nyc_map.R
+++ nyc_map.R
@@ -1,6 +1,6 @@
 
 
-nyc_map <- get_map(location = c(lon = -74.00, lat = 40.78), maptype = "terrain", zoom = 11)
+nyc_map <- get_map(location = c(lon = -73.99, lat = 40.74), maptype = "terrain", zoom = 11)
 ggmap(nyc_map)
 register_google(key = "AIzaSyB62vo0Ry0KhRaMYc4LW0z2mEF7l25s4LU")
 r <- GET('http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson')
