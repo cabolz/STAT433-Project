f754e5967a4998931c6ad0754037c4283b3e791b
Caitlin Bolz <75763155+cabolz@users.noreply.github.com>
2021-05-04 12:55:16 -0500

    commented out first app

--- app.R
+++ app.R
@@ -35,44 +35,32 @@ choropleth = map_data("county") %>%
   filter(County == "New York" | County == "Kings" | County == "Queens" | 
            County == "Bronx" | County == "Richmond")
 
-ui <- fluidPage(
-  titlePanel("NYC Uber Pickup"),
-  sidebarLayout(
-    sidebarPanel(
-      helpText("data from NYC Taxi & Limousine Commission"),
-      sliderInput("range","May 2014",value=c(1,31),min=1,max=31),
-      selectInput("var","Which borough would you like to see?",borough)),
-    mainPanel(plotlyOutput(outputId = "countyChoropleth")))
-)
-
-server <- function(input, output) {
-  output$countyChoropleth <- renderPlotly({
-    
-    plot = ggplot(choropleth, aes(long, lat, group = County)) +
-      geom_polygon(aes(fill = County), colour = alpha("black", 1/2), size = 0.1)  +
-      
-      labs (
-        title = "Number of Uber Pickups by County") +
-      theme(
-        panel.background = element_rect(fill = "#FFFFFF"),
-        panel.grid = element_blank(),
-        axis.ticks = element_blank(),
-        axis.text = element_blank(),
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank()
-      ) 
-    
-    # Remove ability to pan and zoom, set plot dimensions
-    ggplotly(plot, width = 700, tooltip = c("County", "Species", "Density")) %>% 
-      config(displayModeBar = T) %>% 
-      layout(xaxis=list(fixedrange=F),
-             yaxis=list(fixedrange=F))
-  })
-  
-  
-}
-
-shinyApp(ui, server)
+# ui <- fluidPage(
+#   titlePanel("NYC Uber Pickup"),
+#   sidebarLayout(
+#     sidebarPanel(
+#       helpText("data from NYC Taxi & Limousine Commission"),
+#       sliderInput("range","May 2014",value=c(1,31),min=1,max=31),
+#       selectInput("var","Which borough would you like to see?",borough)),
+#     mainPanel(plotlyOutput(outputId = "countyChoropleth"))))
+# server <- function(input, output) {
+#   output$countyChoropleth <- renderPlotly({
+#     plot = ggplot(choropleth, aes(long, lat, group = County)) +
+#       geom_polygon(aes(fill = County), colour = alpha("black", 1/2), size = 0.1)  +
+#       labs (
+#         title = "Number of Uber Pickups by County") +
+#       theme(
+#         panel.background = element_rect(fill = "#FFFFFF"),
+#         panel.grid = element_blank(),
+#         axis.ticks = element_blank(),
+#         axis.text = element_blank(),
+#         axis.title.x = element_blank(),
+#         axis.title.y = element_blank()) 
+#     ggplotly(plot, width = 700, tooltip = c("County", "Species", "Density")) %>% 
+#       config(displayModeBar = T) %>% 
+#       layout(xaxis=list(fixedrange=F),
+#              yaxis=list(fixedrange=F))})}
+# shinyApp(ui, server)
 
 r <- GET('http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson')
 nyc_neighborhoods <- readOGR(content(r,'text'), 'OGRGeoJSON', verbose = F)
