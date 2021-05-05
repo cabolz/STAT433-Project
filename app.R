7b43d38b96217b487450be915043385ceb8ed59a
Matt <mcvoss@wisc.edu>
2021-05-05 13:33:03 -0500

    commit

--- app.R
+++ app.R
@@ -7,10 +7,9 @@ library(stringr)
 library(tidyverse)
 
 borough<- c("Manhattan","Brooklyn","Queens","Bronx","Staten Island")
-uw = read_csv("UberandWeather.csv")
-View(uw)
 fullData<- read_csv("fullData.csv")
-View(fullData)
+
+
 fullData = fullData %>%
   mutate( 
     borough = case_when(
@@ -44,10 +43,9 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       helpText("data from NYC Taxi & Limousine Commission"),
-      sliderInput("range","May 2014",value=c(1,31),min=1,max=31),
-      selectInput("var","Which borough would you like to see?",borough)),
-    mainPanel(plotlyOutput(outputId = "countyChoropleth")))
-)
+      sliderInput("range","May 2014",value=c(1,31),min=1,max=31)),
+      #selectInput("var","Which borough would you like to see?",borough)),
+    mainPanel(plotlyOutput(outputId = "countyChoropleth"))))
 
 server <- function(input, output) {
   output$countyChoropleth <- renderPlotly({
@@ -82,3 +80,29 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
+library(plotly)
+library(rjson)
+r1 = 'http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson'
+geojson <- rjson::fromJSON(file=r1)
+full = fullData %>% 
+  transmute(borough,neighborhood,day) %>% 
+  group_by(borough,day,neighborhood) %>% 
+  summarize(num_points= n(),borough,day)
+full = distinct(full)
+full
+fig <- plot_ly() 
+fig <- fig %>% add_trace(
+  type="choroplethmapbox",
+  geojson=geojson,
+  locations=full$neighborhood,
+  z=full$num_points,
+  colorscale="Viridis",
+  featureidkey = "properties.neighborhood"
+)
+fig <- fig %>% layout(
+  mapbox=list(
+    style="carto-positron",
+    zoom =9,
+    center=list(lon = -73.98,lat =  40.75))
+)
+fig
