655399b8b45e20daa70045e8c880893b7b3553b8
Caitlin Bolz <75763155+cabolz@users.noreply.github.com>
2021-05-05 21:22:48 -0500

    partial update

--- app.R
+++ app.R
@@ -10,44 +10,106 @@ library(tidyverse)
 
 ui <- fluidPage(
   titlePanel("NYC Uber Pickup"),
-  sidebarLayout(
-    sidebarPanel(
-      helpText("data from NYC Taxi & Limousine Commission"),
-    sliderInput("range", label = h3("Slider Range for May 2014"), min = 1, 
-                max = 31, value = c(10, 20))),
-    mainPanel(plotlyOutput(outputId = "countyChoropleth"))))
+  plotlyOutput(outputId = "Choropleth1"),
+  plotlyOutput(outputId = "Choropleth2"),
+  plotlyOutput(outputId = "Choropleth3"),
+  plotlyOutput(outputId = "Choropleth4"),
+  plotlyOutput(outputId = "Choropleth5"))
 
 server <- function(input, output) {
-  output$countyChoropleth <- renderPlotly({
-
-    plot = ggplot(choropleth, aes(long, lat, group = borough)) +
+  output$Choropleth1 <- renderPlotly({
+    plot = ggplot(choropleth1, aes(long, lat, group = borough)) +
       geom_polygon(aes(fill = pickups), colour = alpha("black", 1/2), size = 0.1)  +
       labs (
-        title = "Number of Species by County") +
+        title = "Number of Pickups by County/Borough") +
       theme(
         panel.background = element_rect(fill = "#FFFFFF"),
         panel.grid = element_blank(),
         axis.ticks = element_blank(),
         axis.text = element_blank(),
         axis.title.x = element_blank(),
-        axis.title.y = element_blank()
-      )+
-      scale_fill_viridis_c(option = "magma")
-
-    # Remove ability to pan and zoom, set plot dimensions
+        axis.title.y = element_blank()) 
     ggplotly(plot, width = 700, tooltip = c("borough", "pickups")) %>%
       plotly::config(displayModeBar = T) %>%
       layout(xaxis=list(fixedrange=F),
              yaxis=list(fixedrange=F))
   })
-  #choose a borough to be displayed on Choropleth map
-  # this.borough = reactive(switch(input$var,
-  #                       "Manhattan" = tab$`New York`,
-  #                       "Brooklyn" = tab$Kings,
-  #                       "Queens" = tab$Queens,
-  #                       "Bronx" = tab$Bronx,
-  #                       "Staten Island" = tab$Richmond))
-
+  
+  output$Choropleth2 <- renderPlotly({
+    plot = ggplot(choropleth2, aes(long, lat, group = borough)) +
+      geom_polygon(aes(fill = pickups), colour = alpha("black", 1/2), size = 0.1)  +
+      labs (
+        title = "Number of Pickups by County/Borough") +
+      theme(
+        panel.background = element_rect(fill = "#FFFFFF"),
+        panel.grid = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+      scale_fill_viridis_c()
+    ggplotly(plot, width = 700, tooltip = c("borough", "pickups")) %>%
+      plotly::config(displayModeBar = T) %>%
+      layout(xaxis=list(fixedrange=F),
+             yaxis=list(fixedrange=F))
+  })
+  
+  output$Choropleth3 <- renderPlotly({
+    plot = ggplot(choropleth3, aes(long, lat, group = borough)) +
+      geom_polygon(aes(fill = pickups), colour = alpha("black", 1/2), size = 0.1)  +
+      labs (
+        title = "Number of Pickups by County/Borough") +
+      theme(
+        panel.background = element_rect(fill = "#FFFFFF"),
+        panel.grid = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+      scale_fill_viridis_c(option = "magma")
+    ggplotly(plot, width = 700, tooltip = c("borough", "pickups")) %>%
+      plotly::config(displayModeBar = T) %>%
+      layout(xaxis=list(fixedrange=F),
+             yaxis=list(fixedrange=F))
+  })
+  
+  output$Choropleth4 <- renderPlotly({
+    plot = ggplot(choropleth4, aes(long, lat, group = borough)) +
+      geom_polygon(aes(fill = pickups), colour = alpha("black", 1/2), size = 0.1)  +
+      labs (
+        title = "Number of Pickups by County/Borough") +
+      theme(
+        panel.background = element_rect(fill = "#FFFFFF"),
+        panel.grid = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+      scale_fill_viridis_c(option = "magma")
+    ggplotly(plot, width = 700, tooltip = c("borough", "pickups")) %>%
+      plotly::config(displayModeBar = T) %>%
+      layout(xaxis=list(fixedrange=F),
+             yaxis=list(fixedrange=F))
+  })
+  
+  output$Choropleth5 <- renderPlotly({
+    plot = ggplot(choropleth5, aes(long, lat, group = borough)) +
+      geom_polygon(aes(fill = pickups), colour = alpha("black", 1/2), size = 0.1)  +
+      labs (
+        title = "Number of Pickups by County/Borough") +
+      theme(
+        panel.background = element_rect(fill = "#FFFFFF"),
+        panel.grid = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+      scale_fill_viridis_c(option = "magma")
+    ggplotly(plot, width = 700, tooltip = c("borough", "pickups")) %>%
+      plotly::config(displayModeBar = T) %>%
+      layout(xaxis=list(fixedrange=F),
+             yaxis=list(fixedrange=F))
+  })
 }
 
 shinyApp(ui, server)
